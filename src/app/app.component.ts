36da39ba6c2b266479ee742807518b18fbd025ee
ashrafbadra <ashrafbadra949@gmail.com>
2020-07-16 01:17:55 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'deltatask';
+  radio: any[];
+
+  ngOnInit() {
+    this.radio = [{ station: "Putin FM ", number: '66.6' }, { station: "Dribble FM", number: '101.2' }
+      , { station: "Doge FM ", number: '99.4' }, { station: "Ballads FM ", number: '87.1' },
+    { station: "Maximum FM ", number: '142.2' }]
+
+  }
+  slide(event) {
+    document.getElementById("demo").innerHTML = event.target.innerHTML;
+    $(".finish").show();
+    $(event.target).siblings('.show-one').slideToggle(500)
+    $(event.target).parent().siblings().children('.show-one').slideUp();
+  }
+
 }
